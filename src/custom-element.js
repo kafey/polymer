99c7068d5f2b638713a7d6d162b311bcc8d2dad9
abdurahmanfz <36391767+abdurahmanfz@users.noreply.github.com>
2018-04-09 16:01:32 +0700

    serialize form

---
+++ src/custom-element.js
@@ -0,0 +1,11 @@
+import {Elements as PolymerElements} from "../bower_components/polymer/polymer-element.js"
+
+
+class NameTag extends PolymerElements {
+	static get is() {return "custom-element";}
+	costructor() {
+		super();
+		
+		
+	}
+}
